2044515b993d8fedf5602cd7b4cb98c4af0dd157
Jota Vares <joe@manulith.com>
2016-07-06 09:42:22 -0400

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Virtually Haunted</title>
+
+        <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+
+        <style>
+            html, body {
+                height: 100%;
+                background-color: #000;
+                color:#fff;
+                font-family: 'Indie Flower', cursive;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="title">Get Ready to Meet the Ghost in the Machine...</div>
+        </div>
+    </body>
+</html>
